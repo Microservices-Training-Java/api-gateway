1ca4906538e36b8bdb702a81f925fcc241f1134a
Toanns <nguyensongtoan0302@gmail.com>
2023-06-09 21:39:37 +0700

    implement decode token and forward header to service

--- authen/src/main/java/com/sub/authen/filter/TokenAuthenticationFilterWebFlux.java
+++ authen/src/main/java/com/sub/authen/filter/TokenAuthenticationFilterWebFlux.java
@@ -4,6 +4,7 @@ import com.sub.authen.facade.FacadeService;
 import com.sub.authen.service.AuthTokenService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
@@ -21,7 +22,18 @@ public class TokenAuthenticationFilterWebFlux implements WebFilter {
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
         log.info("Start filter");
-        return null;
+        var headers = exchange.getRequest().getHeaders();
+        var userId = headers.getFirst("user_id");
+        var role = headers.getFirst("role");
+        var language = headers.getFirst("language");
+        var username = headers.getFirst("username");
+
+        exchange.getAttributes().put("userId", userId);
+        exchange.getAttributes().put("role", role);
+        exchange.getAttributes().put("language", language);
+        exchange.getAttributes().put("username", username);
+
+        return chain.filter(exchange);
     }
 }
 
--- gateway/src/main/java/com/example/configuration/GatewayConfiguration.java
+++ gateway/src/main/java/com/example/configuration/GatewayConfiguration.java
@@ -2,10 +2,12 @@ package com.example.configuration;
 
 import com.sub.authen.config.EnableAuthentication;
 import org.aibles.header.configuration.EnableCustomHeader;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.request.RequestContextListener;
+import org.springframework.web.reactive.config.EnableWebFlux;
 
 @EnableAuthentication
 @Configuration
-@EnableCustomHeader
 public class GatewayConfiguration {
 }
